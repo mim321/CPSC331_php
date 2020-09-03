d52531e2b6260aa0651eeb75ccba69da2869ac27
mim321 <45837613+mim321@users.noreply.github.com>
2020-09-03 13:02:48 +0700

    Add files via upload

---
+++ HomeWork3/index.php
@@ -0,0 +1,55 @@
+<header>
+<title>โฮมเพจของข้าพเจ้า:การสร้างตาราง</title>
+<marquee direction ="right">CPSC331_PHP </marquee>
+  <p>....</p>
+</header>
+
+<style>
+header {
+  text-align: center;
+  padding: 0px;
+  background-color: #FFDAB9;
+  color: #000000;
+}
+</style>
+
+<style>
+    @font-face
+        {
+            font-family:TLWGTypewriter;
+            src:url('rsp_tlwgtypewriter.ttf');
+
+        }
+footer 
+	{
+   position:absolute;
+   position:absolute;
+   bottom:0;
+   left:0;
+   width:100%;
+   height:100%
+   height:50px;
+   text-align: center;
+   color: #ffffff;
+   font-weight:bold;
+   background-color: #FFDAB9;
+}
+</head>
+<body>
+    </style>
+    <pre style="font-family:TLWGTypewriter;font-size:40px;">
+        <?php echo "<br/>";
+            echo " ";
+        ?>
+    </pre>
+   <CENTER> 
+   <a href="s0index.php">pro_s0index</a>
+</pre>
+<footer>
+  <p>CONTACT<br>
+  <a href="https://www.facebook.com/mim.suchicha"><img src="8.gif" width="60" height="50" alt=""></a>
+  <a href="https://github.com/mim321"><img src="7.gif" width="60" height="50" alt=""></a>
+  <a href="https://twitter.com/mymimmmm1"><img src="5.gif" width="60" height="50" alt=""></a>
+  <a href="https://www.youtube.com/channel/UCHqdeiHTJPLjayV1gW-2Y7A?view_as=subscriber"><img src="9.gif" width="60" height="50" alt=""></a></p>
+</footer>
+</body>
